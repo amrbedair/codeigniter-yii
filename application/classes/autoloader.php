0d9867962ac7fe207fff1d6a2870b0f83a11c367
Amr Bedair <amr.bedair@gmail.com>
2014-09-17 20:36:34 +0200

    autoloader+

--- application/classes/autoloader.php
+++ application/classes/autoloader.php
@@ -3,43 +3,53 @@
  * 
  * @param string $class_name
  */
-function __autoload($className) {
-	
-	$file = __DIR__.DIRECTORY_SEPARATOR.'classMap.php';
-	if(!file_exists($file)) {
-		file_put_contents($file, "<?php\nreturn array(\n\t// class map\n);");
-		chmod($file, 0644);
-	}
-	
-	$map = require $file;
-	if(!isset($map[$className])) { // not in the map
-		if($f = findClassFile($className)) { // file found
-			$map[$className] = $f;
-			updateMap($map, $file);
-			include $map[$className];
-		} else { // file not found
-			// let CI determine what to do
+if(!function_exists('autoLoader')) {
+	function autoLoader($className) {
+		
+		$file = __DIR__.DIRECTORY_SEPARATOR.'classMap.php';
+		if(!file_exists($file)) {
+			file_put_contents($file, "<?php\nreturn array(\n\t// class map\n);");
+			chmod($file, 0644);
 		}
-	} else {
-		include $map[$className];
+		
+		$map = require $file;
+		if(!isset($map[$className])) { // not in the map
+			if($f = findClassFile($className)) { // file found
+				$map[$className] = $f;
+				updateMap($map, $file);
+				include $map[$className];
+			} else { // file not found
+				// let CI determine what to do
+			}
+		} else {
+			include $map[$className];
+		}
 	}
 }
 
-function findClassFile($className) {
-	$it = new RecursiveDirectoryIterator(APPPATH.'classes');
-	foreach(new RecursiveIteratorIterator($it) as $file) {
-		if(substr($file, -strlen('/'.$className.'.php')) == '/'.$className.'.php')
-			return $file;
-	}
+if(!function_exists('findClassFile')) {
+	function findClassFile($className) {
 	
-	return false;
+		$it = new RecursiveDirectoryIterator(APPPATH.'classes');
+		foreach(new RecursiveIteratorIterator($it) as $file) {
+			if(substr($file, -strlen('/'.$className.'.php')) == '/'.$className.'.php')
+				return $file;
+		}
+		
+		return false;
+	}
 }
 
-function updateMap($map, $file) {
-	$_string = "<?php\nreturn array(\n\t// class map\n";
-	foreach ($map as $key => $value) {
-		$_string .= "\t'".$key."' => '".$value."',\n";
+if(!function_exists('updateMap')) {
+	function updateMap($map, $file) {
+		$_string = "<?php\nreturn array(\n\t// class map\n";
+		foreach ($map as $key => $value) {
+			$_string .= "\t'".$key."' => '".$value."',\n";
+		}
+		$_string .= ');';
+		file_put_contents($file, $_string);
 	}
-	$_string .= ');';
-	file_put_contents($file, $_string);
 }
+
+// register auto loader
+spl_autoload_register('autoLoader');
